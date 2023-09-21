5b73561ce780244290c5e88da748d8bfc608f0b2
Patuco <patricia.beco.dg@gmail.com>
2023-09-21 12:35:40 +0200

    merge dev branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import '../styles/App.scss';
+import '../styles/__app.scss';
 import React, { useState, useEffect } from 'react';
 import Navbar from './Sidebar/Navbar';
 import Project from './Project/Project';
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import taskData from '../services/taskData.json';
 import ProjectDetail from '../pages/ProjectDetail';
 import Home from '../pages/Home';
-import Header from './Header/header';
+import Header from './Header/Header';
 import Options from './Header/Options';
 import ViewOptions from './Header/ViewOptions';
 
@@ -18,8 +18,9 @@ function App() {
   return (
     <>
       <Navbar projects={projects} setProjects={setProjects} />
+      <Home />
       <Routes>
-        <Route path="/">home</Route>
+        <Route path="/"></Route>
         <Route path="/Project/id">Proyecto/id</Route>
       </Routes>
     </>
