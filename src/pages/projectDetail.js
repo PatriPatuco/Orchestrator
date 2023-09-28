dc84533fcda0e4e2fca5099f13c367fa0a076ad0
Natalia <nataliaouteiral@gmail.com>
2023-09-28 17:04:54 +0200

    added p detail

---
+++ src/pages/projectDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Project from '../components/Project/Project';
+
+const ProjectDetail = ({ projectFound }) => {
+  console.log(`${projectFound} "esto es la info del proyecto en detail"`);
+  return <Project project={projectFound} />;
+};
+
+export default ProjectDetail;
