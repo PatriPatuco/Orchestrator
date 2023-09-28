a92940f9f6493d96285a4d93bc5b88c205e68d9e
Natalia <nataliaouteiral@gmail.com>
2023-09-28 17:14:22 +0200

    change to capital letters

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
-import "../styles/__app.scss";
-import React, { useState, useEffect } from "react";
-import Navbar from "./Sidebar/Navbar";
+import '../styles/__app.scss';
+import React, { useState, useEffect } from 'react';
+import Navbar from './Sidebar/Navbar';
 /* import {useLocation, matchPath } from "react-router"; */
-import { Routes, Route, useLocation, matchPath} from "react-router-dom";
-import taskData from "../services/taskData.json";
-import Project from "../components/Project/Project";
-import Home from "../pages/Home";
-import Header from "./Header/Header";
-import Options from "./Header/Options";
-import ViewOptions from "./Header/ViewOptions";
-import ProjectDetail from "../pages/ProjectDetail";
+import { Routes, Route, useLocation, matchPath } from 'react-router-dom';
+import taskData from '../services/taskData.json';
+import Project from '../components/Project/Project';
+import Home from '../pages/Home.js';
+import Header from './Header/Header';
+import ViewOptions from './Header/ViewOptions';
+import ProjectDetail from '../pages/ProjectDetail';
 
 function App() {
   // const [sidebar, setSidebar] = useState(false);
@@ -19,11 +18,13 @@ function App() {
 
   // dynamic routes (projects id)
 
-  const {pathname } = useLocation();
-const routeData = matchPath('project/:id', pathname);
-const projectId = routeData === null ? null : routeData.params.id;
-const projectFound = projects.find((eachProject) => eachProject.id === projectId)
-console.log(projectId)
+  const { pathname } = useLocation();
+  const routeData = matchPath('project/:id', pathname);
+  const projectId = routeData === null ? null : routeData.params.id;
+  const projectFound = projects.find(
+    (eachProject) => eachProject.id === projectId
+  );
+  console.log(projectId);
 
   return (
     <>
@@ -39,7 +40,7 @@ console.log(projectId)
           Proyecto/id
         </Route>
       </Routes>
-    </div>
+    </>
   );
 }
 
