bed9f4eca3abc75a29a3d84365e244cd94b24f04
Patuco <patricia.beco.dg@gmail.com>
2023-10-02 11:46:39 +0200

    change dynanmics route to ProjectDetail

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,42 @@
-import '../styles/__app.scss';
-import React, { useState, useEffect } from 'react';
-import Navbar from './Sidebar/Navbar';
-/* import {useLocation, matchPath } from "react-router"; */
-import { Routes, Route, useLocation, matchPath } from 'react-router-dom';
-import taskData from '../services/taskData.json';
-import Project from '../components/Project/Project';
-import Home from '../pages/Home.js';
-import Header from './Header/Header';
-import ViewOptions from './Header/ViewOptions';
-import ProjectDetail from '../pages/ProjectDetail';
+import "../styles/__app.scss";
+import React, { useState, useEffect } from "react";
+import Navbar from "./Sidebar/Navbar";
+import { Routes, Route, useLocation, useParams, matchPath} from "react-router-dom";
+import taskData from "../services/taskData.json";
+import Project from "../components/Project/Project";
+import Home from "../pages/Home";
+import Header from "./Header/Header";
+/* import Options from "./Header/Options"; */
+import ViewOptions from "./Header/ViewOptions";
+import ProjectDetail from "../pages/ProjectDetail";
 
 function App() {
   // const [sidebar, setSidebar] = useState(false);
 
   const fakedata = [];
   const [projects, setProjects] = useState(taskData);
-  const [list, setList] = useState(false);
+   const [projectDet, setProjectDet] = useState({});
 
-  const showList = () => {};
+   console.log("info guardada", projectDet)
+/*    console.log(projects) */
 
   // dynamic routes (projects id)
 
-  const { pathname } = useLocation();
-  const routeData = matchPath('/project/:id', pathname);
-  const projectId = routeData !== null ? routeData.params.id : null;
-  const projectFound = projects
-    .map((eachp) => eachp.id)
-    .find((value) => value === parseInt(projectId));
-
-  // const idValue = projects
-  //   .map((eachp) => eachp.id)
-  //   .find((value) => value === parseInt(projectId));
-  // console.log(idValue);
-  // console.log(projectId);
-
-  // if (idValue === parseInt(projectId)) {
-  //   return console.log('Olé');
-  // } else {
-  //   console.log('caca');
-  // }
-  // const projectFiltered = projects.filter((p) => {
-  //   return p.id === projectId;
-  // });
-  // console.log(projectFiltered);
-
   return (
-    <div className="main">
-      <Header />
+    <>
       <Navbar projects={projects} setProjects={setProjects} />
-
       <Home projects={projects} />
+
       <Routes>
         <Route path="/" element={<Project />}></Route>
         <Route
           path="/project/:id"
-          element={<ProjectDetail projectFound={projectFound} />}
-        >
-          Proyecto/id
-        </Route>
+          element={
+            <ProjectDetail projects={projects}/>
+          }
+        ></Route>
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/pages/projectDetail.js
+++ src/pages/projectDetail.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import Project from '../components/Project/Project';
+import React from "react";
+import { useParams } from "react-router-dom";
+import Project from "../components/Project/Project";
 
-const ProjectDetail = ({ projectFound }) => {
-  console.log(`${projectFound} "esto es la info del proyecto en detail"`);
-  return <Project project={projectFound} />;
+const ProjectDetail = ({ projects }) => {
+  
+
+  console.log("props de ProjectDetail", projects);
+  const { id } = useParams();
+  console.log("ID de useParams:", id);
+
+  // Verificar si 'projects' está definido antes de buscar el proyecto
+  if (!projects) {
+    return <p>Cargando datos...</p>;
+  }
+
+  const projectId = parseInt(id, 10); // Convierte id a número base 10
+  const projectFound = projects.find(
+    (eachProject) => eachProject.id === projectId
+  );
+
+  /* 
+  // Utiliza 'id' para buscar el proyecto correspondiente
+  const projectFound = projects.find((eachProject) => eachProject.id === id); */
+
+  console.log(projectFound);
+  return (
+    <div>
+      {projectFound ? (
+        <>
+          <h2>Detalles del Proyecto {id}</h2>
+          <Project project={projectFound} />
+        </>
+      ) : (
+        <p>Proyecto no encontrado</p>
+      )}
+    </div>
+  );
 };
 
 export default ProjectDetail;
