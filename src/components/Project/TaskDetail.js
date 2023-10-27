8cd5ac47883d4be2c24e4f17d4fd29083af5d123
Patuco <patricia.beco.dg@gmail.com>
2023-10-27 11:17:52 +0200

    Add styles and custom checkbox to TaskDetail

--- src/components/Project/TaskDetail.js
+++ src/components/Project/TaskDetail.js
@@ -1,40 +1,49 @@
-const TaskDetail = ({showTaskInfo, task}) => {
-
-
+const TaskDetail = ({ showTaskInfo, task }) => {
   console.log(task);
 
-    return (
-      <section className="taskDetail">
-        <div className="taskDetail__content">
-          <span class="material-symbols-outlined">close</span>
-          <h3 className="taskDetail__title">{task.title}</h3>
-          <p className="taskDetail__desc">{task.desc}</p>
+  return (
+    <section className="taskDetail">
+      <div className="taskDetail__container">
+        <h3 className="taskDetail__title">{task.title}</h3>
+        <span
+          class="material-symbols-outlined taskDetail__close-btn"
+          onClick={showTaskInfo}
+        >
+          close
+        </span>
+        <p className="taskDetail__desc">{task.desc}</p>
 
-          <h4 className="taskDetail__info--title">Subtasks</h4>
-          {task.subtask.map((subtask, index) => (
-            <label className="taskDetail__subtask" key={index}>
-              <input type="checkbox" placeholder="e.g. Clean spam mails" />{" "}
-              {subtask.def}
-            </label>
-          ))}
-          <h4 className="taskDetail__info--title">Status</h4>
-          <select>
-            <option>Todo</option>
-            <option>Doing</option>
-            <option>Done</option>
-          </select>
+        <h4 className="taskDetail__info--title">Subtasks</h4>
+        {task.subtask.map((subtask, index) => (
+          <label className="taskDetail__subtask" key={index}>
+            <input className="taskDetail__subtask--input" type="checkbox" />
+            <span class="material-symbols-outlined checked-box">check</span>
+            {subtask.def}
+          </label>
+        ))}
+        <h4 className="taskDetail__info--title">Status</h4>
+        <select className="taskDetail__status">
+          <option>Todo</option>
+          <option>Doing</option>
+          <option>Done</option>
+        </select>
 
-          <h4 className="taskDetail__info--title">Due Date</h4>
-          <input type="datetime-local"></input>
-          <button className="taskDetail__button">Editar</button>
-        </div>
-      </section>
-    );
-}
+        <h4 className="taskDetail__info--title">Due Date</h4>
+        <input type="datetime-local" className="taskDetail__date"></input>
+        <button className="taskDetail__button">
+          <span class="material-symbols-outlined taskDetail__button--i">
+            edit
+          </span>
+          Edit
+        </button>
+      </div>
+    </section>
+  );
+};
 
-
-
-{/* <section className="taskDetail">
+// HTML PARA ADD TASK
+{
+  /* <section className="taskDetail">
   <div className="taskDetail__content">
     <h3 className="taskDetail__title"></h3>
     <label htmlFor="title">
@@ -64,6 +73,7 @@ const TaskDetail = ({showTaskInfo, task}) => {
       />
     </label>
   </div>
-</section>; */}
+</section>; */
+}
 
 export default TaskDetail;
